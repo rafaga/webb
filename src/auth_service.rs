c6b94ae5bec13e57617fdfd267cc4a05a7c4ecab
Rafael Amador <rafael.amador@gmail.com>
2023-03-30 23:25:13 -0600

    testing

--- src/auth_service.rs
+++ src/auth_service.rs
@@ -3,18 +3,16 @@ use std::pin::Pin;
 use std::task::{Context, Poll};
 use std::net::SocketAddr;
 
+use std::sync::Arc;
 use hyper::service::Service;
 use hyper::{Request, Response};
 use hyper::{Body, Method,  Server, StatusCode};
-use tokio::task;
 
 static CONFIRM: &[u8] = b"<html><head><title>Telescope login</title><style>body{font-family: monospace;background-color: gray;color: whitesmoke;}</style></head><body><h1>Telescope</h1><p>Logged in!, now you can close this window safetly.</p></body></html>";
 static NOT_VALID: &[u8] = b"Invalid Request";
 
 pub struct AuthService{
-    characterid: usize,
-    code: String,
-    state: String,
+    tx: Arc<tokio::sync::oneshot::Sender<(String,String)>>,
 }
 
 impl Service<Request<Body>> for AuthService {
@@ -31,23 +29,33 @@ impl Service<Request<Body>> for AuthService {
             (&Method::GET, "/login") => { 
                 let pnq = req.uri().path_and_query();
                 if let Some(params) = pnq.unwrap().query() {
+                    let mut message:(String,String)=(String::new(),String::new()); 
                     let parameters = params.split("&").collect::<Vec<&str>>();
                     for param in parameters {
                         let p = param.split("=").collect::<Vec<&str>>();
                         match p[0] {
                             "code" => {
-                                self.code = p[1].to_string();
+                                message.0 = p[1].to_string();
                             },
                             "state" => {
-                                self.state = p[1].to_string();
+                                message.1 = p[1].to_string();
                             },
                             _ => ()
                         }
                     }
-                    Ok(Response::builder()
-                    .status(StatusCode::OK)
-                    .body(CONFIRM.into())
-                    .unwrap())
+                    if !message.0.is_empty() && !message.1.is_empty() {
+                        let tx = Arc::clone(&self.tx);
+                        tx.send(message);
+                        Ok(Response::builder()
+                        .status(StatusCode::OK)
+                        .body(CONFIRM.into())
+                        .unwrap())
+                    } else {
+                        Ok(Response::builder()
+                        .status(StatusCode::UNPROCESSABLE_ENTITY)
+                        .body(NOT_VALID.into())
+                        .unwrap())
+                    }
                 } else {
                     Ok(Response::builder()
                     .status(StatusCode::UNPROCESSABLE_ENTITY)
@@ -66,18 +74,13 @@ impl Service<Request<Body>> for AuthService {
 }
 
 struct MakeSvc {
-    pub characterid: usize,
-    pub code: String, // String
-    pub state: String, // String
-
+    tx: Arc<tokio::sync::oneshot::Sender<(String,String)>>,
 }
 
 impl MakeSvc {
-    pub fn new(characterid: usize) -> Self {
+    pub fn new(sender: Arc<tokio::sync::oneshot::Sender<(String,String)>>) -> Self {
         MakeSvc {
-            characterid,
-            code: String::new(),
-            state: String::new(),
+            tx: sender
         }
     }
 }
@@ -92,10 +95,8 @@ impl<T> Service<T> for MakeSvc {
     }
 
     fn call(&mut self, _: T) -> Self::Future {
-        let characterid = self.characterid;
-        let code = self.code.clone();
-        let state = self.state.clone();
-        let fut = async move { Ok(AuthService{ characterid, code, state}) };
+        let tx = Arc::clone(&self.tx);
+        let fut = async move { Ok(AuthService{ tx }) };
         Box::pin(fut)
     }
 }
@@ -103,14 +104,17 @@ impl<T> Service<T> for MakeSvc {
 #[tokio::main]
 pub async fn open_auth_service() -> Result<bool, Box<dyn std::error::Error + Send + Sync>> {
     let addr: SocketAddr = ([127, 0, 0, 1], 56123).into();
+    let (tx, rx) = tokio::sync::oneshot::channel::<(String,String)>();
+    let shared_tx = Arc::new(tx);
+    let server = Server::bind(&addr)
+        .serve(MakeSvc::new(Arc::clone(&shared_tx)))
+        .with_graceful_shutdown(async {
+            rx.await.ok();
+        });
     
-    let server = Server::bind(&addr).serve(MakeSvc::new(0));
-    println!("Listening on http://{}", addr);
-    
-    let handle = tokio::spawn(async move{
-        server.await
-    });
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 
-    let k = handle.await;
     Ok(true)
 }
