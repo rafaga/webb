f290a8d9c5d2c3b5a3bfa81809ca885eeb1defc7
Rafael Amador <rafael.amador@gmail.com>
2023-08-20 23:08:04 -0600

    adding reactor to get_player_photo function

--- src/esi.rs
+++ src/esi.rs
@@ -212,6 +212,7 @@ impl<'a> EsiManager<'a> {
         }
     }
 
+    #[tokio::main]
     pub async fn get_player_photo(url: &str) -> Result<Option<Vec<u8>>,hyper::Error> {
         let https = HttpsConnector::new();
         let client = Client::builder()
